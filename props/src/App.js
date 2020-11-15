295f382976a895aa0f68b7787531c97a16d03ca6
Cho Junseo <chojuneseo1@gmail.com>
2020-11-15 23:28:29 +0900

    props [REACT]

---
+++ props/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './MyComponent.js';
+import MyComponent from './MyComponent.js';
+
+const App = () => {
+  return <MyComponent>리액트</MyComponent>;
+};
+
+export default App;
