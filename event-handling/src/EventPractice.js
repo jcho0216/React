027545888f40f2da73fb36e06ed545e208956d32
Cho Junseo <chojuneseo1@gmail.com>
2020-11-22 23:01:35 +0900

    onKeyPress

--- event-handling/src/EventPractice.js
+++ event-handling/src/EventPractice.js
@@ -6,6 +6,31 @@ class EventPractice extends Component {
         message: ''
     }
 
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+        [e.target.name]: e.target.value
+        });
+    }
+
+    handleClick() {
+        alert(this.state.username + ': ' + this.state.message);
+        this.setState({
+            username: '',
+            message: ''
+        });
+    }
+    
+    handleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+            this.handleClick();
+        }
+    }
     render() {
         return (
             <div>
@@ -13,24 +38,13 @@ class EventPractice extends Component {
                 <input
                 type="text"
                 name="message"
-                placeholder="아무거나 입력해보세요"
+                placeholder="아무거나 입력해주세요"
                 value={this.state.message}
-                onChange={
-                    (e)=> {
-                        this.setState({
-                            message: e.target.value
-                        })
-                    }
-                }/>
+                onChange={this.handleChange}
+                onKeyPress={this.handleKeyPress}
+                />
 
-                <button onClick={() => {
-                    alert(this.state.message);
-                    this.setState({
-                        message:''
-                    });
-                }}>
-                확인
-                </button>
+                <button onClick={this.handleClick}>확인</button>
             </div>
         );
     }
