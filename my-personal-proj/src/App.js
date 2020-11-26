8da349115479ad160e6d8dc17fad62dba706bc7c
Cho Junseo <chojuneseo1@gmail.com>
2020-11-26 22:49:09 +0900

    데이터 추가, 삭제 구현 [REACT]

--- my-personal-proj/src/App.js
+++ my-personal-proj/src/App.js
@@ -4,7 +4,10 @@ import Iteration from './Iteration';
 
 const App = () => {
   return(
+    <>
+    <h2>내용을 입력해주세요!<br/>삭제하려면 더블클릭 해주세요!</h2>
     <Iteration />
+    </>
   );
 }
   
--- my-personal-proj/src/Iteration.js
+++ my-personal-proj/src/Iteration.js
@@ -12,6 +12,7 @@ const Iteration = () => {
     const [nextId, setNextId] = useState(5);
 
     const onChange = e => setInputText(e.target.value);
+
     const onClick = () => {
         const nextNames = names.concat({
             id: nextId,
@@ -22,7 +23,14 @@ const Iteration = () => {
         setInputText('');
     };
 
-    const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+    const onRemove = id => {
+        const nextNames = names.filter(name=> name.id !== id);
+        setNames(nextNames);
+    };
+
+    const namesList = names.map(name => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>
+    ));
 
     return (
         <>
