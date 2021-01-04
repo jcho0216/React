a1dd564c2165723b930db510e260198d800c20e8
Cho Junseo <chojuneseo1@gmail.com>
2021-01-04 23:48:12 +0900

    Hooks recap
    Hooks recap

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+import React, { useState } from 'react';
+import Counter from './Counter';
+import Info from './Info';
+import ContextSample from './ContextSample';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <SassComponent />
-      </div>
-    );
-  }
+const App = () => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+    <div>
+      <button onClick={()=>{
+          setVisible(!visible);
+        }}>
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+        {visible && <Info/>}
+        {visible && <Counter/>}
+        {visible && <ContextSample/>}
+    </div>
+      
+    </>
+  );
 }
 
 export default App;
