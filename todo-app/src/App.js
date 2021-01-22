c6024620e430b1165cb9375511db8fcbf892bd87
Cho Junseo <chojuneseo1@gmail.com>
2021-01-22 22:33:26 +0900

    To Do List [REACT]
    To Do List insert function implemented

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import TodoTemplate from './TodoTemplate';
 import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '야두리스트',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert/>
-      <TodoList />
+      <TodoInsert  onInsert={onInsert}/>
+      <TodoList todos={todos}/>
     </TodoTemplate>
   );
 };
