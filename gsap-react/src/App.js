4e61104550a7de6c2951a7d82272052d70a24f5c
Cho Junseo <chojuneseo1@gmail.com>
2021-01-27 16:55:58 +0900

    Greensock (GSAP) 사용해보기
    gsap을 이용하여 커지는 공 애니메이션을 만들었다.

--- gsap-react/src/App.js
+++ gsap-react/src/App.js
@@ -1,52 +1,61 @@
-import React, { useEffect, useRef } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
 import { TweenMax, Power3} from 'gsap';
-
+import './App.css';
 function App() {
-  let logoItem = useRef(null);
-  let textItem = useRef(null);
+  let app = useRef(null);
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
 
-  console.log(logoItem);
+  const handleExpand = () => {
+    TweenMax.to(circle, .8, {width: 200, height: 200, ease: Power3.easeOut});
+    setExpand(true);
+  }
+
+  const handleShrink = () => {
+    TweenMax.to(circle, .8, {width: 75, height: 75, ease: Power3.easeOut});
+    setExpand(false);
+  }
+
+  const handleExpandRed = () => {
+    TweenMax.to(circleRed, .8, {width: 200, height: 200, ease: Power3.easeOut});
+    setExpand(true);
+  }
+
+  const handleShrinkRed = () => {
+    TweenMax.to(circleRed, .8, {width: 75, height: 75, ease: Power3.easeOut});
+    setExpand(false);
+  }
+
+  const handleExpandBlue = () => {
+    TweenMax.to(circleBlue, .8, {width: 200, height: 200, ease: Power3.easeOut});
+    setExpand(true);
+  }
+
+  const handleShrinkBlue = () => {
+    TweenMax.to(circleBlue, .8, {width: 75, height: 75, ease: Power3.easeOut});
+    setExpand(false);
+  }
+
+
+  const [ expand, setExpand] = useState(false);
 
   useEffect(() => {
-    console.log(logoItem);
-    TweenMax.to(
-      logoItem,
-      .8,
-      {
-        opacity: 1,
-        y: -20,
-        ease: Power3.easeOut,
-        delay: 0.2
-      }
-    )
-    TweenMax.to(
-      textItem,
-      .8,
-      {
-        opacity: 1,
-        y: -20,
-        ease: Power3.easeOut,
-        delay: 0.2
-      }
-    )
+    console.log(circle, circleRed, circleBlue);
+    TweenMax.to(app, 0, {css: {visibility: 'visible'}})
+    // TweenMax.from(circle, .8, {opacity: 0, x: 40, ease: Power3.easeOut})
+    // TweenMax.from(circleRed, .8, {opacity: 0, x: 40, ease: Power3.easeOut, delay: 0.2})
+    // TweenMax.from(circleBlue, .8, {opacity: 0, x: 40, ease: Power3.easeOut, delay: 0.4})
+    TweenMax.staggerFrom([circle, circleRed, circleBlue], .8, {opacity: 0, x: 40, ease: Power3.easeOut}, 0.2);
   }, []);
   return (
-    <div className="App">
+    <div className="App" ref={el => app = el}>
       <header className="App-header">
-        <img ref={el => {logoItem = el}} src={logo} className="App-logo" alt="logo" />
-        <p ref={el => {textItem = el}}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="circle-container">
+          <div className="circle" ref={el=>circle = el} onClick={expand !== true ? handleExpand : handleShrink}></div>
+          <div className="circle red" ref={el=>circleRed = el} onClick={expand !== true ? handleExpandRed : handleShrinkRed}></div>
+          <div className="circle blue" ref={el=>circleBlue = el} onClick={expand !== true ? handleExpandBlue : handleShrinkBlue}></div>
+        </div>
       </header>
     </div>
   );
