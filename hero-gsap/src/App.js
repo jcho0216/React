1b1b4fedd92d248a5c78aece4898d19ec0cb6762
Cho Junseo <chojuneseo1@gmail.com>
2021-01-27 23:49:06 +0900

    Greensock (GSAP) 사용하여 멋있는 애니메이션 구현
    샤라랄라

---
+++ hero-gsap/src/App.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useRef } from 'react';
+import {TweenMax, TimelineLite, Power3} from 'gsap';
+import './App.scss';
+//assets
+import imageGirl from './images/girl.webp';
+import imageBoy from './images/boy.webp';
+import arrow from './images/arrow.svg';
+
+
+
+
+function App() {
+    let tl = new TimelineLite({delay: .8});
+    //img vars
+    let app = useRef(null);
+    let images = useRef(null);
+
+    //content vars
+    let content = useRef(null);
+
+    useEffect(() => {
+      //img vars
+      const girlImage = images.firstElementChild;
+      const boyImage = images.lastElementChild;
+
+      const headlineFirst = content.children[0].children[0];
+      const headlineSecond = headlineFirst.nextSibling;
+      const headlineThird = headlineSecond.nextSibling;
+      const contentP = content.children[1];
+      const contentButton = content.children[2];
+
+      console.log(headlineFirst);
+      console.log(headlineFirst.children);
+
+      TweenMax.to(app, 0, {css: {visibility: 'visible'}});
+      //images animation
+      tl.from(girlImage, 1.2, {y: 1280, ease: Power3.easeOut}, 'Start')
+      .from(girlImage.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
+      .from(boyImage, 1.2, {y: 1280, ease: Power3.easeOut}, .2)
+      .from(boyImage.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
+
+      //content animation
+      tl.staggerFrom([headlineFirst.children, headlineSecond.children, headlineThird.children], 1, {
+        y: 44,
+        ease: Power3.easeOut,
+        delay: .8
+      }, .15, 'Start')
+      .from(contentP, 1, {y:20, opacity: 0, ease: Power3.easeOut}, 1.4)
+      .from(contentButton, 1, {y:20, opacity: 0, ease: Power3.easeOut}, 1.6)
+    }, [tl])
+
+
+  return (
+    <div className="hero" ref={el => app = el}>
+      <div className="container">
+        <div className="hero-inner">
+          <div className="hero-content">
+            <div className="hero-content-inner" ref={el => content = el}>
+              <h1>
+                <div className="hero-content-line">
+                  <div className='hero-content-line-inner'>Relieving the burden</div>
+                </div>
+                <div className="hero-content-line">
+                  <div className='hero-content-line-inner'>of disease caused</div>
+                </div>
+                <div className="hero-content-line">
+                  <div className='hero-content-line-inner'>by behavior.</div>
+                </div>
+              </h1>
+              <p>Better treats serious cardiometabolic diseases to transform
+                lives and reduce healthcare utilization through the use of
+                digital therapeutics.</p>
+              <div className="button-row">
+                <button className="explore-button">explore
+                  <div className="arrow-icon">
+                    <img src={arrow} alt="arrow" />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="hero-images">
+            <div className="hero-images-inner" ref={el => images = el}>
+              <div className="hero-image girl">
+                <img src={imageGirl} alt="girl" />
+              </div>
+              <div className="hero-image boy">
+                <img src={imageBoy} alt="boy" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
