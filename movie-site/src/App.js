8df4813bc4a4514cb1bad290539fe253c1df1ab2
Jun Joy <chojuneseo1@gmail.com>
2021-03-08 23:14:49 +0900

    movie-site
    nomad coder movie-site tutorial

---
+++ movie-site/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import axios from 'axios';
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount() {
+    axios.get("https://yts.mx/api/v2/list_movies.json")
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+  }
+}
+export default App;
