86d31b9597423cb300ba87a52552999f32aa70f7
Jun Joy <chojuneseo1@gmail.com>
2021-03-17 23:04:01 +0900

    Context Api study
    context API

---
+++ context-tutorial/src/components/LeftPane.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sends from './Sends';
+
+const Receives = () => {
+    return (
+        <div className="pane">
+            <Sends />
+        </div>
+    )
+}
+
+export default Receives;
