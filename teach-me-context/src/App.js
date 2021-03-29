72cf21af6e34177d5478cb59dfb342f1b27e9de5
Jun Joy <chojuneseo1@gmail.com>
2021-03-29 19:38:43 +0900

    Context API Practice
    Context API tutorial finished

--- teach-me-context/src/App.js
+++ teach-me-context/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
+//add new context
+const myContext = React.createContext();
+
+//create provider
+class MyProvider extends Component {
+  state = {
+    name: "Junseo",
+    age: 100,
+    cool: true,
+  };
+
+  render() {
+    return (
+      <myContext.Provider
+        value={{
+          state: this.state,
+          GrowAYearOlder: () => this.setState({
+            age: this.state.age + 1
+          })
+        }}
+      >
+        {this.props.children}
+      </myContext.Provider>
+    );
+  }
+}
+
+const Family = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="family">
+      <Person />
     </div>
   );
+};
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <myContext.Consumer>
+          {(context) => (
+            <div>
+              <p>Age = {context.state.age}</p>
+              <p>Name = {context.state.name}</p>
+              <button onClick={context.GrowAYearOlder}>+1</button>
+            </div>
+          )}
+        </myContext.Consumer>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    name: "Junseo, Cho",
+    age: 100,
+    cool: true,
+  };
+  render() {
+    return (
+      <MyProvider>
+        <div>
+          <p>I'm the App</p>
+          <Family />
+        </div>
+      </MyProvider>
+    );
+  }
 }
 
 export default App;
