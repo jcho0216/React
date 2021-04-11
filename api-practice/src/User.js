33ba8d67b3bc8e4102523d84b58923a0bb0d4125
Jun Joy <chojuneseo1@gmail.com>
2021-04-11 23:33:35 +0900

    api-practice
    api practice

--- api-practice/src/User.js
+++ api-practice/src/User.js
@@ -1,41 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const User = () => {
-    const [users, setUsers] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(()=>{
-        const fetchUser = async () => {
-            try {
-                setError(null);
-                setUsers(null);
-                setLoading(null);
-                const response = await axios.get(
-                    'https://jsonplaceholder.typicode.com/users'
-                );
-                setUsers(response.data);
-            } catch (e) {
-                setError(e);
-            }
-            setLoading(false);
-        };
-        fetchUser();
-    }, []);
+  const fetchUser = async () => {
+    try {
+      setError(null);
+      setUsers(null);
+      setLoading(null);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(response.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+  
+  useEffect(() => {
+    
+    fetchUser();
+  }, []);
 
-    if (loading) return <div>로딩중..</div>;
-    if (error) return <div>에러가 발생했습니다</div>;
-    if (!users) return null;
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return null;
 
-    return (
-        <ul>
-        {users.map(user => (
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
           <li key={user.id}>
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
-    );
-}
+      <button onClick={fetchUser}>다시 불러오기</button>
+    </div>
+  );
+};
 
 export default User;
