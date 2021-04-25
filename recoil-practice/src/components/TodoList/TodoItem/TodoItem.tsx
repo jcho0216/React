1d51f9fc58378f3a8dd3fe23f6e5ddb7c0b9d2d9
Jun Joy <chojuneseo1@gmail.com>
2021-04-25 23:10:14 +0900

    recoil-practice
    todoItem implemented

--- recoil-practice/src/components/TodoList/TodoItem/TodoItem.tsx
+++ recoil-practice/src/components/TodoList/TodoItem/TodoItem.tsx
@@ -1,16 +1,65 @@
 import React, { useCallback, useState } from "react";
-import { ITodoTypes } from '../../recoil/todo';
-import { FaPen } from 'react-icons/fa';
-import { MdClose } from 'react-icons/md';
+import { ITodoTypes } from "../../recoil/todo";
+import { FaPen } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
 import { SetterOrUpdater } from "recoil";
 
 interface PropTypes {
-    id: number;
-    contents: string;
+  id: number;
+  contents: string;
 
-    onComplete: (id: number) => void;
-    onDelete: (id: number) => void;
-    
-    todos: ITodoTypes[];
-    setTodos: SetterOrUpdater<ITodoTypes[]>;
+  onComplete: (id: number) => void;
+  onDelete: (id: number) => void;
+
+  todos: ITodoTypes[];
+  setTodos: SetterOrUpdater<ITodoTypes[]>;
 }
+
+const TodoItem = () => ({
+  id,
+  contents,
+  isCompleted,
+  onComplete,
+  onDelete,
+  todos,
+  setTodos,
+}: PropTypes): JSX.Element => {
+  const [isModal, setIsModal] = useState<boolean>(false);
+  const [modifyContents, setModifyContents] = useState<string>("");
+
+  const onModify = useCallback((): void => {
+    setIsModal(true);
+    setModifyContents(contents);
+  }, [contents]);
+
+  const onModifyTodo = useCallback((): void => {
+    if (!modifyContents.trim()) {
+      return;
+    }
+
+    setTodos(todos.map((todo: ITodoTypes) => {
+      return todo.id === id ? { ...todo, contents: modifyContents } : todo;
+    }));
+
+    setIsModal(false);
+  }, [id, modifyContents, setTodos, todos]);
+
+  return (
+    <>
+      <div className='TodoItem'>
+        <div
+          title={contents}
+          className={isCompleted ? 'TodoItem-Completed' : ''}
+          onClick={() => onComplete(id)}
+        >
+          {contents}
+        </div>
+
+        <div className='TodoItem-Icons'>
+          <FaPen className='TodoItem-Icons-Pen' onClick={onModify} />
+          <MdClose className='TodoItem-Icons-Close' onClick={() => onDelete(id)} />
+        </div>
+      </div>
+    </>
+  );
+};
